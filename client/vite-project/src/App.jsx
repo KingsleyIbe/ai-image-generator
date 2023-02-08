2c19c601199175b49dc6eae94a4640bca900b900
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-02-08 20:06:29 +0100

    Add pages for home and create post

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -18,8 +18,15 @@ function App() {
         <Link to="/">
           <img src={logo} alt="Project Logo" className="w-28 object-contain" />
         </Link>
-        <Link to="/create-post" className="font-inter font-medium">Create Post</Link>
+        <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create Post</Link>
       </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[cal(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+
+      </main>
     </BrowserRouter>
   );
 }
