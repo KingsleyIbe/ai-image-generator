850a673f4c78df33e9937b85592606ea76fd7a83
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-02-09 13:38:24 +0100

    Update Home page

--- client/vite-project/src/pages/Home.jsx
+++ client/vite-project/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { Card, FormField, Loader } from '../components';
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
+  const [searchText, setSearchText] = useState('jdjd');
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -16,6 +17,23 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <FormField />
+      </div>
+      <div className="mt-10">
+        {loading ? (
+          <div className="flex justify-center items-center">
+            <Loader />
+          </div>
+        ) : (
+          <>
+            {searchText && (
+            <h2 className="font-medium text-[#666e75] text-xl mb-3">
+              Showing result for
+              {' '}
+              <span className="text-[#222328]">{searchText}</span>
+            </h2>
+            )}
+          </>
+        )}
       </div>
     </section>
   );
