dcd649d11d186237e136c5512db05aa05ab04926
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-02-10 22:55:38 +0100

    Update home page

--- client/vite-project/src/pages/CreatePost.jsx
+++ client/vite-project/src/pages/CreatePost.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { preview } from '../assets';
+import { getRandomPrompt } from '...utils';
 
 const CreatePost = () => (
   <div>CreatePost</div>
