e4c46ad3f7612daf5955c5f20d319fa5cb9b28a5
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-02-15 12:03:13 +0100

    Update dalleRoutes file

--- server/index.js
+++ server/index.js
@@ -19,25 +19,25 @@ app.get('/', async (req, res) => {
   res.send('Hello World');
 });
 
-router.route('/').post(async(req, res) => {
-  try {
-    const { prompt } = req.body;
+// router.route('/').post(async(req, res) => {
+//   try {
+//     const { prompt } = req.body;
 
-    const aiResponse = await openai.createImage({
-      prompt,
-      n: 1,
-      size: '1024x1024',
-      response_format: 'b64_json',
-    });
+//     const aiResponse = await openai.createImage({
+//       prompt,
+//       n: 1,
+//       size: '1024x1024',
+//       response_format: 'b64_json',
+//     });
 
-    const image = aiResponse.data.data[0].b64_json;
+//     const image = aiResponse.data.data[0].b64_json;
 
-    res.status(200).json({ photo: image });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error?.response.data.error.message)
-  }
-});
+//     res.status(200).json({ photo: image });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).send(error?.response.data.error.message)
+//   }
+// });
 
 const startServer = async () => {
 
@@ -50,3 +50,5 @@ const startServer = async () => {
 };
 
 startServer();
+
+// export default router;
