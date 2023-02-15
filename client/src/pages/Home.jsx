ae474f47bb33cd6e8b6f7956f59e7b4603908af3
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-02-15 13:34:03 +0100

    Implement share photo with community feature

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
 import { Card, FormField, Loader } from '../components';
 
 const RenderCards = ({ data, title }) => {
-  if (data.length > 0) return data.map((post) => <Card key={post._id} {...post} />);
+  if (data?.length > 0) return data.map((post) => <Card key={post?._id} {...post} />);
   return (
     <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
   );
@@ -12,6 +13,31 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+  }, []);
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -45,7 +71,7 @@ const Home = () => {
           {searchText ? (
             <RenderCards data={[]} title="No search results found" />
           ) : (
-            <RenderCards data={[]} title="No post found" />
+            <RenderCards data={allPosts} title="No post found" />
 
           )}
 
